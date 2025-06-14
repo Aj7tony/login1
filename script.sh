131864e58c665468c076a011bab3ea774d973ede
aj7tony7 <ajreddy7aj2@gmail.com>
2025-06-14 15:09:48 +0530

    script created

---
+++ script.sh
@@ -0,0 +1,8 @@
+shell scripts
+
+1. sudo apt update -y
+2. sudo apt install nginx -y
+3. sudo rm -rf /var/www/html/index.nginx-debian.html
+4 sudo git clone https://github.com/Aj7tony/login1.git /var/www/html
+
+
